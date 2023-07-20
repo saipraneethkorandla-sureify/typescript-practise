b2a16cada3f5fdadd94603a0a8f6a89f24ecc3e3
saipraneethkorandla-sureify <saipraneeth.korandla@sureify.com>
2023-07-20 17:08:22 +0530

    added gitignore

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Function_1 = require("../src/Function");
+const Function_1 = require("./Function");
 console.log((0, Function_1.addition)(1, 2));
 console.log((0, Function_1.substraction)(3, 2));
 console.log((0, Function_1.multiplication)(4, 5));
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {addition, substraction, multiplication, division} from './src/Function';
+import {addition, substraction, multiplication, division} from './Function';
 
 console.log(addition(1,2))
 console.log(substraction(3,2))
